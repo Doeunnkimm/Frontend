7149ecfe42281beecb3f8922e7c67198f8b5596f
Doeunnkimm <doeunnkimm@gmail.com>
2023-02-03 14:02:52 +0900

    :four_leaf_clover: Javascript : 프엔 스터디 level4 풀이코드 추가

--- study-quiz/week1/level4/level4.js
+++ study-quiz/week1/level4/level4.js
@@ -4,5 +4,57 @@
 let totalPage = 80;
 let currentPageIndx = 0;
 let currentPage = new URLSearchParams(location.search).get('page') || 1;
-
 */
+
+let totalPage = 80;
+let currentPageIndex = 0;
+let currentPage = new URLSearchParams(location.search).get('page') || 1;
+
+const page_list = document.querySelector('.page_list');
+const pre_btn = document.querySelector('.pre');
+const next_btn = document.querySelector('.next');
+
+for (let i = 1; i <= 10; i++) {
+  const page = document.createElement('span');
+  page.innerText = i * (currentPageIndex + 1);
+
+  page.addEventListener('click', () => {
+    currentPage = Number(currentPage);
+    let nowPage = Math.floor(currentPage / 10);
+
+    currentPageIndex =
+      Number([...String(currentPage).split('')].join('')) - (currentPage % 10);
+
+    if (page.innerText != '10') {
+      window.location.href = window.location.href = `/level4/level4.html?page=${
+        currentPageIndex + Number(page.innerText)
+      }`;
+    } else {
+      window.location.href = window.location.href = `/level4/level4.html?page=${
+        (nowPage + 1) * 10
+      }`;
+    }
+  });
+
+  page_list.appendChild(page);
+}
+
+pre_btn.addEventListener('click', () => {
+  if (currentPage > 10) {
+    currentPageIndex -= 10;
+
+    window.location.href = `/level4/level4.html?page=${
+      Number(currentPage) - 10
+    }`;
+  }
+});
+
+next_btn.addEventListener('click', () => {
+  if (currentPage < totalPage) {
+    currentPageIndex += 10;
+
+    window.location.href = `/level4/level4.html?page=${
+      Number(currentPage) + 10
+    }`;
+  }
+});

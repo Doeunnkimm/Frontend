5ec0e65d543902cd8a8130b71d265bc0d0303fa5
Doeunnkimm <doeunnkimm@gmail.com>
2023-02-03 15:15:00 +0900

    :four_leaf_clover: Javascript : 프엔 스터디 level4 풀이코드 수정

--- study-quiz/week1/level4/level4.js
+++ study-quiz/week1/level4/level4.js
@@ -7,32 +7,37 @@ let currentPage = new URLSearchParams(location.search).get('page') || 1;
 */
 
 let totalPage = 80;
-let currentPageIndex = 0;
+let currentPageIndex = 1;
 let currentPage = new URLSearchParams(location.search).get('page') || 1;
 
 const page_list = document.querySelector('.page_list');
 const pre_btn = document.querySelector('.pre');
 const next_btn = document.querySelector('.next');
 
+let nowPage;
+if (Number(currentPage) % 10 != 0) {
+  nowPage = Math.floor(currentPage / 10);
+} else {
+  nowPage = Math.floor(currentPage / 10) - 1;
+}
+
 for (let i = 1; i <= 10; i++) {
-  const page = document.createElement('span');
-  page.innerText = i * (currentPageIndex + 1);
+  const page = document.createElement('button');
+
+  const realPageIndex = nowPage * 10 + i;
+  page.innerText = realPageIndex;
 
   page.addEventListener('click', () => {
-    currentPage = Number(currentPage);
-    let nowPage = Math.floor(currentPage / 10);
+    console.log('nowPage -->', nowPage);
+    console.log('realPageIndex --> ', realPageIndex);
+    console.log('currentPageIndex --> ', currentPageIndex);
 
-    currentPageIndex =
-      Number([...String(currentPage).split('')].join('')) - (currentPage % 10);
+    currentPageIndex = i;
 
-    if (page.innerText != '10') {
-      window.location.href = window.location.href = `/level4/level4.html?page=${
-        currentPageIndex + Number(page.innerText)
-      }`;
+    if (currentPageIndex != 10) {
+      window.location.href = `/level4/level4.html?page=${realPageIndex}`;
     } else {
-      window.location.href = window.location.href = `/level4/level4.html?page=${
-        (nowPage + 1) * 10
-      }`;
+      window.location.href = `/level4/level4.html?page=${realPageIndex}`;
     }
   });
 
@@ -40,21 +45,15 @@ for (let i = 1; i <= 10; i++) {
 }
 
 pre_btn.addEventListener('click', () => {
-  if (currentPage > 10) {
-    currentPageIndex -= 10;
-
-    window.location.href = `/level4/level4.html?page=${
-      Number(currentPage) - 10
-    }`;
+  if (nowPage > 0) {
+    window.location.href = `/level4/level4.html?page=${(nowPage - 1) * 10 + 1}`;
+    nowPage--;
   }
 });
 
 next_btn.addEventListener('click', () => {
-  if (currentPage < totalPage) {
-    currentPageIndex += 10;
-
-    window.location.href = `/level4/level4.html?page=${
-      Number(currentPage) + 10
-    }`;
+  if (nowPage < totalPage / 10) {
+    window.location.href = `/level4/level4.html?page=${(nowPage + 1) * 10 + 1}`;
+    nowPage++;
   }
 });

b73c2e54a8cd5da5228dfa7f76ac8638185e1434
Doeunnkimm <doeunnkimm@gmail.com>
2023-06-02 11:03:34 +0900

    :closed_book: Storybook : PropTypes 추가해보기

--- my-study-react/src/storybook-pratice/Components/TaskList/TaskList.js
+++ my-study-react/src/storybook-pratice/Components/TaskList/TaskList.js
@@ -71,11 +71,11 @@ export default TaskList;
  *
  * Type의 propTypes에서 정의한 것을 재사용하자
  */
-TaskList.PropTypes = {
+TaskList.propTypes = {
   /** Checks if it`s in loading state */
   loading: PropTypes.bool,
   /** The list of tasks */
-  tasks: PropTypes.arrayOf(Task.PropTypes.task).isRequired,
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   /** Event to change the task to pinned */
   onPinTask: PropTypes.func,
   /** Event to change the task to archived */

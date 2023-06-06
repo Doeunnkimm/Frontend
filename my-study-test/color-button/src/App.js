0f622bf25f882c5b37dbcc34525ee7ab6808f6aa
Doeunnkimm <doeunnkimm@gmail.com>
2023-06-06 14:27:33 +0900

    :space_invader: Test : 초기 상태 테스트, 버튼 클릭 테스트

--- my-study-test/color-button/src/App.js
+++ my-study-test/color-button/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState('red');
+
+  const onClickButton = () => {
+    const newColor = color === 'red' ? 'blue' : 'red';
+    setColor(newColor);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button style={{ backgroundColor: color }} onClick={onClickButton}>
+        Change to {color === 'red' ? 'blue' : 'red'}
+      </button>
     </div>
   );
 }
--- my-study-test/color-button/src/App.test.js
+++ my-study-test/color-button/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, logRoles, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
+  // const { container } = render(<App />);
+  // logRoles(container);
   render(<App />);
-  const linkElement = screen.getByText(/learn testing library/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // 버튼을 클릭
+  fireEvent.click(colorButton);
+
+  // 배경 색이 파란색으로 바뀌는 것을 expect
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // 눌렸으므로 버튼 텍스트도 Change to red가 되는 것을 expect
+  expect(colorButton).toHaveTextContent('Change to red');
 });

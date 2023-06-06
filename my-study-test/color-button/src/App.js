27ba48da038b68c1caad98725e6147d09b8ea819
Doeunnkimm <doeunnkimm@gmail.com>
2023-06-06 16:28:44 +0900

    :space_invader: Test : 체크박스 체크 시 버튼 비활성화/활성화 테스트

--- my-study-test/color-button/src/App.js
+++ my-study-test/color-button/src/App.js
@@ -2,17 +2,27 @@ import React, { useState } from 'react';
 
 function App() {
   const [color, setColor] = useState('red');
+  const [isChecked, setIsChecked] = useState(false);
+  const newColor = color === 'red' ? 'blue' : 'red';
 
   const onClickButton = () => {
-    const newColor = color === 'red' ? 'blue' : 'red';
     setColor(newColor);
+  };
+
+  const onChangeCheckbox = e => {
+    setIsChecked(e.target.checked);
   };
 
   return (
     <div>
-      <button style={{ backgroundColor: color }} onClick={onClickButton}>
-        Change to {color === 'red' ? 'blue' : 'red'}
+      <button
+        style={{ backgroundColor: color }}
+        onClick={onClickButton}
+        disabled={isChecked}
+      >
+        Change to {newColor}
       </button>
+      <input type="checkbox" onChange={onChangeCheckbox} />
     </div>
   );
 }
--- my-study-test/color-button/src/App.test.js
+++ my-study-test/color-button/src/App.test.js
@@ -22,3 +22,38 @@ test('button turns blue when clicked', () => {
   // 눌렸으므로 버튼 텍스트도 Change to red가 되는 것을 expect
   expect(colorButton).toHaveTextContent('Change to red');
 });
+
+/**
+ * 체크박스가 체크되면 버튼이 비활성화 동작 관련 테스팅
+ */
+
+// 초기 상태 테스팅
+test('initial conditions', () => {
+  render(<App />);
+
+  // 버튼이 활성화 상태로 시작하는지를 확인
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  // toBeEnabled : 활성화를 확인할 수 있는 매처
+  expect(colorButton).not.toBeDisabled();
+
+  // 체크박스가 체크 안 된 상태로 시작하는지 확인
+  const checkbox = screen.getByRole('checkbox');
+  // not : 단언을 부정하기 위한 jest 단언
+  expect(checkbox).not.toBeChecked();
+});
+
+// 체크 박스를 체크하면 버튼이 비활성화 상태 테스팅
+test('Disable the button by checking the check box', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  const checkbox = screen.getByRole('checkbox');
+
+  // 체크박스 체크
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+
+  // 두 번째 클릭  (체크 해제)
+  fireEvent.click(checkbox);
+  expect(colorButton).not.toBeDisabled();
+});

ad9dbf4c8013e06710ed9ba3aa3b1929c283a763
Doeunnkimm <doeunnkimm@gmail.com>
2023-06-27 19:11:29 +0900

    :stars: NextJS : Custom Error Page
    Error 코드별로 원하는 error message가 포함된 컴포넌트를 노출시킬 수 있다.

---
+++ my-study-nextjs/blog/pages/404.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+  return (
+    <>
+      <div>내용을 찾을 수 없습니다. (URL을 확인해주세요)</div>
+      <Link href={'/'}>홈으로</Link>
+    </>
+  )
+}

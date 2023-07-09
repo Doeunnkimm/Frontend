6beefb7eabc761366239aac3ef0fe53859cadbe4
Doeunnkimm <doeunnkimm@gmail.com>
2023-07-09 23:22:56 +0900

    :pencil2: React : Yup & React Hook Form - schema 부분 관심사 분리

--- my-study-react/src/yupANDhook-form/ControllerTest.js
+++ my-study-react/src/yupANDhook-form/ControllerTest.js
@@ -1,46 +1,26 @@
+import * as FORM_SCHEMA from './constants/form.schema'
 import * as yup from 'yup'
 
 import FormInputText from './ControllerForm'
-import { REGEX } from './constants/regex.const'
 import { useForm } from 'react-hook-form'
 import { useState } from 'react'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 export default function ControllerTest() {
+  const { email, password, password_confirm, phone, age } = FORM_SCHEMA
   const [data, setData] = useState({ email: '', phone: '', age: '' })
 
   const schema = yup.object().shape({
-    email: yup
-      .string()
-      .matches(REGEX.email, {
-        message: '이메일 형식에 알맞게 입력해주세요',
-        excludeEmptyString: true,
-      })
-      .required('이메일을 입력해주세요'),
-    password: yup
-      .string()
-      .min(8, '8자 이상 입력해주세요')
-      .max(16, '16자 이상 입력해주세요')
-      .matches(REGEX.password, {
-        message: '영문자, 숫자, 특수문자를 포함해서 입력해주세요',
-      })
-      .required('비밀번호를 입력해주세요'),
-    password_confirm: yup
-      .string()
-      .oneOf([yup.ref('password')], '입력한 비밀번호와 일치하지 않습니다'),
-    phone: yup
-      .string()
-      .matches(REGEX.phone, { message: '휴대폰 번호를 정확히 입력해주세요' })
-      .required('휴대폰 번호를 입력해주세요'),
-    age: yup.string().required('나이를 입력해주세요'),
+    email,
+    password,
+    password_confirm,
+    phone,
+    age,
   })
   const { control, handleSubmit } = useForm({ resolver: yupResolver(schema) })
 
   const onSubmitSignUp = (data) => {
-    console.log({ data })
-
     const { email, phone, age } = data
-
     setData({ email, phone, age })
   }
 
---
+++ my-study-react/src/yupANDhook-form/constants/form.schema.js
@@ -0,0 +1,31 @@
+import * as yup from 'yup'
+
+import { REGEX } from './regex.const'
+
+export const email = yup
+  .string()
+  .matches(REGEX.email, {
+    message: '이메일 형식에 알맞게 입력해주세요',
+    excludeEmptyString: true,
+  })
+  .required('이메일을 입력해주세요')
+
+export const password = yup
+  .string()
+  .min(8, '8자 이상 입력해주세요')
+  .max(16, '16자 이상 입력해주세요')
+  .matches(REGEX.password, {
+    message: '영문자, 숫자, 특수문자를 포함해서 입력해주세요',
+  })
+  .required('비밀번호를 입력해주세요')
+
+export const password_confirm = yup
+  .string()
+  .oneOf([yup.ref('password')], '입력한 비밀번호와 일치하지 않습니다')
+
+export const phone = yup
+  .string()
+  .matches(REGEX.phone, { message: '휴대폰 번호를 정확히 입력해주세요' })
+  .required('휴대폰 번호를 입력해주세요')
+
+export const age = yup.string().required('나이를 입력해주세요')

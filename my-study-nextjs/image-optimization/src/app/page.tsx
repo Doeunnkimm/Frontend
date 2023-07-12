3f14712e1505ed5f100b90783362bfb4d83e7638
Doeunnkimm <doeunnkimm@gmail.com>
2023-07-12 09:30:17 +0900

    :stars: NextJS : 이미지 최적화 - 반응형 이미지, 이미지 압축

--- my-study-nextjs/image-optimization/src/app/page.tsx
+++ my-study-nextjs/image-optimization/src/app/page.tsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ position: 'relative', width: '300px', height: '300px' }}>
       <Image
         src='/images/test.png'
         alt='test'
-        width={300}
-        height={300}
+        // width={300}
+        // height={300}
+        layout='fill'
+        sizes='(max-width: 768px) 50vw,
+         (max-width: 1024px) 100vw'
         placeholder='blur'
         blurDataURL='image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=='
       />

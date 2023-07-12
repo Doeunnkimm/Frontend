12d5750c79912d185606c224c4f4702afe0f09e3
Doeunnkimm <doeunnkimm@gmail.com>
2023-07-12 23:36:32 +0900

    :stars: NextJS : next13버전과 이전버전 - Image 컴포넌트 비교해보기

--- my-study-nextjs/image-optimization/src/app/page.tsx
+++ my-study-nextjs/image-optimization/src/app/page.tsx
@@ -1,19 +1,37 @@
 import Image from 'next/image'
+import LegacyImage from 'next/legacy/image'
 
 export default function Home() {
   return (
-    <div style={{ position: 'relative', width: '300px', height: '300px' }}>
+    <main>
+      {/* <div style={{ position: 'relative', width: '300px', height: '300px' }}> */}
+      <label>Image(13)</label>
       <Image
         src='/images/test.png'
         alt='test'
-        // width={300}
-        // height={300}
-        layout='fill'
-        sizes='(max-width: 768px) 50vw,
-         (max-width: 1024px) 100vw'
-        placeholder='blur'
-        blurDataURL='image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=='
+        width={300}
+        height={300}
+        //   layout='fill'
+        //   sizes='(max-width: 768px) 50vw,
+        // (max-width: 1024px) 100vw'
+        //   placeholder='blur'
+        //   blurDataURL='image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=='
       />
-    </div>
+      {/* </div> */}
+      {/* <div style={{ position: 'relative', width: '300px', height: '300px' }}> */}
+      <label>Legacy</label>
+      <LegacyImage
+        src='/images/test.png'
+        alt='test'
+        width={300}
+        height={300}
+        //   layout='fill'
+        //   sizes='(max-width: 768px) 50vw,
+        // (max-width: 1024px) 100vw'
+        //   placeholder='blur'
+        //   blurDataURL='image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg=='
+      />
+      {/* </div> */}
+    </main>
   )
 }

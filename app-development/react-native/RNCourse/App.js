395622a1dcfbbf59cfadafa3af8786852c87fe01
Doeunnkimm <doeunnkimm@gmail.com>
2024-05-18 23:59:53 +0900

    chore: 스타일링

--- app-development/react-native/RNCourse/App.js
+++ app-development/react-native/RNCourse/App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style='auto' />
+      <Text style={{ margin: 16, borderWidth: 2, borderColor: '#FF0000', padding: 16 }}>인라인으로 스타일링</Text>
+      <Text style={styles.dummyText}>StyleSheet으로 스타일링</Text>
+      <Button title='Button은 title 속성을 통해 입력해요' />
     </View>
   );
 }
 
+// NOTE: StyleSheet을 이용하면 스타일 프로퍼티 키값 자동완성을 제공
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: 'red',
+    padding: 16,
   },
 });
--- app-development/react-native/RNCourse/README.md
+++ app-development/react-native/RNCourse/README.md
@@ -10,3 +10,9 @@ $ expo init {프로젝트명}
 ```bash
 $ pnpm start
 ```
+
+## 스타일링
+
+- [공식문서 - Style](https://reactnative.dev/docs/style)
+- [공식문서 - Colors](https://reactnative.dev/docs/colors)
+- [공식문서 - View](https://reactnative.dev/docs/view)

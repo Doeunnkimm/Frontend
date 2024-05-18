3ce37b664b2d45d03ce58f215894ea2fa47e6633
Doeunnkimm <doeunnkimm@gmail.com>
2024-05-19 00:51:18 +0900

    chore: iOS와  Android 스타일링 차이

--- app-development/react-native/RNCourse/App.js
+++ app-development/react-native/RNCourse/App.js
@@ -29,7 +29,11 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         {courseGoals.map((courseGoal, index) => (
-          <Text key={index}>{courseGoal}</Text>
+          <View
+            key={index}
+            style={styles.goalItem}>
+            <Text style={styles.goalText}>{courseGoal}</Text>
+          </View>
         ))}
       </View>
     </View>
@@ -61,10 +65,27 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 4,
   },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+  },
+  goalText: {
+    color: 'white',
+  },
 });
 
 /**
  * @NOTE
  * - web에서 하듯이 display: 'flex'를 하게 되면 안 먹음
  * - flexDirection: 'row' === display: 'flex'랑 같은 역할
+ */
+
+/**
+ * @NOTE
+ * - iOS에서와 Android에서 스타일링이 다르게 적용되는 경우가 존재한다.
+ * - 예를 들어, Text에 borderRadius를 적용할 경우
+ * - 안드에서는 background에 잘 적용되지만, 아요에서는 잘 적용 X
+ * - 이럴 경우를 대비하여 상위에 <View>를 감싸 사용해야 하는 경우가 존재한다.
  */

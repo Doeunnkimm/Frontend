42c10c1d530f85a5637d6ce8997ca27c001bfc77
Doeunnkimm <doeunnkimm@gmail.com>
2024-05-19 22:05:48 +0900

    chore: Android & iOS용 스타일링

--- app-development/react-native/game-app/screens/StartGameScreen.js
+++ app-development/react-native/game-app/screens/StartGameScreen.js
@@ -1,9 +1,9 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -12,3 +12,18 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});

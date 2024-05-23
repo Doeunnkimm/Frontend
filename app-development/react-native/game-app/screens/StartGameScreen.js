1d9c68787619cd411e32c6e2d9bcad263e14e14b
Doeunnkimm <doeunnkimm@gmail.com>
2024-05-23 20:05:10 +0900

    chore: 그라데이션 배경 & 이미지 배경 추가해보기

--- app-development/react-native/game-app/screens/StartGameScreen.js
+++ app-development/react-native/game-app/screens/StartGameScreen.js
@@ -10,8 +10,14 @@ const StartGameScreen = () => {
         keyboardType='number-pad'
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -20,10 +26,12 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'stretch',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#4e0329',
+    backgroundColor: '#3b021f',
     borderRadius: 8,
     elevation: 4,
     shadowColor: 'black',
@@ -41,5 +49,11 @@ const styles = StyleSheet.create({
     marginVertical: 8,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
